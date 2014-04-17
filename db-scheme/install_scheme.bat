cb11a43844584946f9cb92382bfbc8e895e8857d
cimolus <cimolus@mail.ru>
2014-04-17 14:30:41 +0300

    install script

--- db-scheme/install_scheme.bat
+++ db-scheme/install_scheme.bat
@@ -1,7 +1,5 @@
-sqlplus %1/%2@%3 @1-create-tablespace.sql
-sqlplus %1/%2@%3 @2-create-user.sql
-sqlplus %1/%2@%3 @3-create-tables.sql
-sqlplus %1/%2@%3 @4-create-relations.sql
+sqlplus %1/%2@%3 @install_script.sql
+
 
 
 
---
+++ db-scheme/install_script.sql
@@ -0,0 +1,10 @@
+@@drop_indexes.sql
+@@drop_sequences.sql
+@@drop_tables.sql
+@@clean-scheme.sql
+
+@@1-create-tablespace.sql
+@@2-create-user.sql
+@@3-create-tables.sql
+@@4-create-sequences.sql
+@@5-create-indexes.sql
